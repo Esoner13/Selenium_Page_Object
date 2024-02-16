60ba9f1c288d3cf4f8186ec0b64965da767c1016
German <netz13@mail.ru>
2024-02-16 18:18:43 +0200

    correction variable

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,20 +8,20 @@ class ProductPage(BasePage):
         basket_link.click()
 
     def should_be_product_name(self):
-        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Product name is wrong"
-        name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
-        n = name.text
-        nm = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_SM)
-        m = nm.text
-        assert n == m, "Name is wrong"
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Product name is not same"
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        name = product_name.text
+        name_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_SM)
+        nim = name_in_message.text
+        assert name == nim, "Name is wrong"
 
     def should_be_basket_price(self):
-        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Basket price is wrong"
-        price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
-        p = price.text
-        pr = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_SM)
-        r = pr.text
-        assert p == r, "Price is wrong"
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Basket price is not same"
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        price = product_price.text
+        price_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_SM)
+        pim = price_in_message.text
+        assert price == pim, "Price is wrong"
 
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
